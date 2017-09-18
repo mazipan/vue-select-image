c57cdac7f37e97bd538db150a7d7d773d59f41bc
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-09-18 10:07:47 +0700

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="app">
     <h1>{{ msg }}</h1>
 
     <div>      
--- src/main-dist.js
+++ src/main-dist.js
@@ -1,3 +1,11 @@
 import VueSelectImage from './VueSelectImage.vue'
 
+const plugin = {
+  install: Vue => {
+    Vue.component(VueSelectImage.name, VueSelectImage)
+  }
+}
+
+VueSelectImage.install = plugin.install
+
 export default VueSelectImage
