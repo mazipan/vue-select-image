92e8ce29c736c62a0a2d8296878994aee3e55371
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-09-26 11:48:21 +0700

    move to .vue file

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,41 @@
 <template>
-  <div id="app" class="app">
-    <h1>{{ msg }}</h1>
+  <div id="app" class="VueSelectImage">
+    <header role="header" class="site__header header"><div class="header__wrapper"><div class="header__brand"> <a href="https://mazipan.github.io/demo/"> <img src="https://mazipan.github.io/images/irfan-maulana.jpg" alt="Irfan Maulana" title="Irfan Maulana"> </a><div class="header__title"> Irfan Maulana | Demo</div></div></div> </header>
 
-    <div>      
-      <h2>Single Selection</h2>      
-      <vue-select-image :dataImages="dataImages" 
-                        @onSelectImage="onSelectImage">
-      </vue-select-image>
+    <div class="app">
+      <h1>{{ msg }}</h1>
+
+      <div>      
+        <h2>Single Selection</h2>      
+        <vue-select-image :dataImages="dataImages" 
+                          @onSelectImage="onSelectImage">
+        </vue-select-image>
+        <div>
+          <h5>Image Selected : 
+            <span v-if="imageSelected.id !== ''">
+              <span>id = {{ imageSelected.id }}</span>
+            </span> 
+          </h5>            
+        </div>
+      </div>
+
       <div>
-        <h5>Image Selected : 
-          <span v-if="imageSelected.id !== ''">
-            <span>id = {{ imageSelected.id }}</span>
-          </span> 
-        </h5>            
+        <h2>Multiple Selection</h2>
+        <vue-select-image :dataImages="dataImages" 
+                          :is-multiple="true" 
+                          @onSelectMultipleImage="onSelectMultipleImage">
+        </vue-select-image>
+        <div>
+          <h5>Mutiple Image Selected : 
+            <span v-for="(imgSelected, index) in imageMultipleSelected" :key="index">
+              <span>id = {{ imgSelected.id }}, </span>
+            </span>
+          </h5> 
+        </div>
       </div>
     </div>
 
-    <div>
-      <h2>Multiple Selection</h2>
-      <vue-select-image :dataImages="dataImages" 
-                        :is-multiple="true" 
-                        @onSelectMultipleImage="onSelectMultipleImage">
-      </vue-select-image>
-      <div>
-        <h5>Mutiple Image Selected : 
-          <span v-for="(imgSelected, index) in imageMultipleSelected" :key="index">
-            <span>id = {{ imgSelected.id }}, </span>
-          </span>
-        </h5> 
-      </div>
-    </div>
+    <div class="grid__row content centered"><h2>Contribute</h2><p> Feel free to fork <i class="fa fa-code-fork"></i> on <a href="https://github.com/mazipan/vue-select-image" target="_blank">GitHub <i class="fa fa-github"></i></a> if you have any features <i class="fa fa-cart-plus"></i> or bugs <i class="fa fa-bug"></i>!</p></div><div class="grid__row content centered"><h2>Contact Developer</h2><p><a href="https://github.com/mazipan"> <img src="https://img.shields.io/badge/mazipan-Github-lightgrey.svg?maxAge=3600" alt="Github"> </a><a href="mailto:mazipanneh@gmail.com"> <img src="https://img.shields.io/badge/mazipanneh-Email-yellow.svg?maxAge=3600" alt="Email"> </a><a href="https://mazipanneh.com/blog/"> <img src="https://img.shields.io/badge/mazipanneh-Blog-brightgreen.svg?maxAge=3600" alt="Blog"> </a><a href="https://facebook.com/mazipanneh"> <img src="https://img.shields.io/badge/mazipanneh-Facebook-blue.svg?maxAge=3600" alt="Facebook"> </a><br/><a href="https://twitter.com/Maz_Ipan"> <img src="https://img.shields.io/badge/Maz_Ipan-Twitter-55acee.svg?maxAge=3600" alt="Twitter"> </a><a href="https://id.linkedin.com/in/irfanmaulanamazipan"> <img src="https://img.shields.io/badge/irfanmaulanamazipan-Linkedin-0077b5.svg?maxAge=3600" alt="Linkedin"> </a><a href="https://www.slideshare.net/IrfanMaulana21"> <img src="https://img.shields.io/badge/IrfanMaulana21-Slideshare-0077b5.svg?maxAge=3600" alt="Slideshare"> </a></p></div><div class="grid__row content centered"><p> Copyright © 2017 <a href="https://mazipanneh.com/blog/">Irfan Maulana</a>, All Rights Reserved.</p></div>
   </div>
 </template>
 
