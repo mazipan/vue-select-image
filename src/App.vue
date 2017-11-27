afdf1b000312314f70d880340688fad176a184b3
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-11-27 16:17:08 +0700

    move to .js file

--- src/App.vue
+++ src/App.vue
@@ -39,50 +39,7 @@
   </div>
 </template>
 
-<script>
-import VueSelectImage from './VueSelectImage.vue'
-
-export default {
-  name: 'app',
-  components: { VueSelectImage },
-  data () {
-    return {
-      msg: 'Vue Select Image By Irfan Maulana',
-      imageSelected: {
-        id: '',
-        src: '',
-        alt: ''
-      },
-      imageMultipleSelected: [],
-      dataImages: [{
-        id: '1',
-        src: 'https://raw.githubusercontent.com/mazipan/vue-select-image/master/src/assets/logo-1.png',
-        alt: 'jQuery'
-      }, {
-        id: '2',
-        src: 'https://raw.githubusercontent.com/mazipan/vue-select-image/master/src/assets/logo-2.png',
-        alt: 'Angular'
-      }, {
-        id: '3',
-        src: 'https://raw.githubusercontent.com/mazipan/vue-select-image/master/src/assets/logo-3.png',
-        alt: 'Vue.js'
-      }, {
-        id: '4',
-        src: 'https://raw.githubusercontent.com/mazipan/vue-select-image/master/src/assets/logo-4.png',
-        alt: 'React'
-      }]
-    }
-  },
-  methods: {
-    onSelectImage: function (data) {
-      this.imageSelected = Object.assign({}, this.imageSelected, data)
-    },
-    onSelectMultipleImage: function (data) {
-      this.imageMultipleSelected = data
-    }
-  }
-}
-</script>
+<script src="./app.js"></script>
 
 <style lang="scss">
 #app {
---
+++ src/vue-select-image.js
@@ -0,0 +1,44 @@
+
+export default {
+  name: 'vue-select-image',
+  props: {
+    dataImages: {
+      type: Array,
+      default: []
+    },
+    isMultiple: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      dataImagesLocal: [],
+      singleSelected: {
+        id: ''
+      },
+      multipleSelected: []
+    }
+  },
+  created () {
+    this.dataImagesLocal = this.dataImages
+  },
+  methods: {
+    onSelectImage: function (objectImage) {
+      this.singleSelected = Object.assign({}, this.singleSelected, objectImage)
+      this.$emit('onSelectImage', objectImage)
+    },
+    onSelectMultipleImage: function (objectImage) {
+      this.dataImagesLocal = this.dataImagesLocal.map((item) => {
+        if (objectImage.id === item.id) {
+          item.selected = !item.selected
+        }
+        return item
+      });
+      this.multipleSelected = this.dataImagesLocal.filter((item) => {
+        return !!item.selected
+      })
+      this.$emit('onSelectMultipleImage', this.multipleSelected)
+    }
+  }
+}
